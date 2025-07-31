7ec89a16ec0f1256381c2920380aee17f2d4affa
Tomer Barnea <tomerb360@gmail.com>
2025-07-31 20:45:29 +0300

    Create build-apkeditor.sh

---
+++ build-apkeditor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Initializing submodules..."
+git submodule update --init --recursive
+
+echo "Building APKEditor..."
+cd libs/APKEditor
+
+# Make gradlew executable
+chmod +x gradlew
+
+# Build APKEditor
+./gradlew build
+
+# Copy the built JAR to a more accessible location
+cd ../..
+cp libs/APKEditor/app/build/libs/*.jar libs/APKEditor/APKEditor.jar
+
+echo "APKEditor built successfully!"
